4123ba42c7d50dd87d1337c63d8579aaf278840b
asyrawih <hasyrawi@gmail.com>
2023-10-21 15:44:20 +0800

    bootstrap user repo

---
+++ backend/repository/mysql/user.go
@@ -0,0 +1,45 @@
+package mysql
+
+import (
+	"context"
+
+	"github.com/jackc/pgx/v5"
+
+	"bacakomik/record/entity"
+)
+
+// UserRepository struct
+type UserRepository struct {
+	conn *pgx.Conn
+}
+
+// NewUserRepository function
+func NewUserRepository(conn *pgx.Conn) *UserRepository {
+	return &UserRepository{
+		conn: conn,
+	}
+}
+
+// Create Data
+func (us *UserRepository) Create(ctx context.Context, data *entity.User) error {
+	panic("not implemented") // TODO: Implement
+}
+
+// Get All Data
+func (us *UserRepository) GetAll(ctx context.Context) []*entity.User {
+	panic("not implemented") // TODO: Implement
+}
+
+// Retrive One Data
+func (us *UserRepository) GetOne(ctx context.Context, id int) *entity.User {
+	panic("not implemented") // TODO: Implement
+}
+
+func (us *UserRepository) Delete(ctx context.Context, id int) bool {
+	panic("not implemented") // TODO: Implement
+}
+
+// Update Data
+func (us *UserRepository) Update(ctx context.Context, data *entity.User, id int) error {
+	panic("not implemented") // TODO: Implement
+}
