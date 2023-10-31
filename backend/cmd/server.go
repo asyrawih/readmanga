cd70cfaa048f006b3527c1f7b10549696efbc870
asyrawih <hasyrawi@gmail.com>
2023-10-31 12:17:28 +0800

    add cli when running main app

--- backend/cmd/server.go
+++ backend/cmd/server.go
@@ -3,7 +3,7 @@ package cmd
 import (
 	"context"
 	"fmt"
-	"log"
+	"os"
 
 	"bacakomik/config"
 	"bacakomik/http"
@@ -13,6 +13,9 @@ import (
 	"bacakomik/repository"
 	"bacakomik/repository/mysql"
 	"bacakomik/service"
+
+	"github.com/rs/zerolog/log"
+	"github.com/urfave/cli/v2"
 )
 
 func RunServer(config *config.Config) {
@@ -20,7 +23,7 @@ func RunServer(config *config.Config) {
 	ctx := context.Background()
 	c, err := repository.Connect(ctx, DSN)
 	if err != nil {
-		log.Fatal(err)
+		log.Err(err).Msg("")
 	}
 
 	mr := mysql.NewMangaRepository(c)
@@ -41,5 +44,38 @@ func RunServer(config *config.Config) {
 	mediaHTTP := media.NewMediaHTTPServer(h, meds, config)
 	http.RegisterHttp(mangaHTTP, chapterHTTP, mediaHTTP)
 
-	h.RunHttpServer(":8000")
+	app := &cli.App{
+		Commands: []*cli.Command{
+			{
+				Name:        "start",
+				Aliases:     []string{"p"},
+				Description: "application running on defined port",
+				Flags: []cli.Flag{
+					&cli.IntFlag{
+						Name:     "port",
+						Value:    8000,
+						Usage:    "p",
+						Aliases:  []string{"p"},
+						Required: true,
+					},
+				},
+				Action: func(ctx *cli.Context) error {
+					p := ctx.Int("port")
+					ports := fmt.Sprintf(":%d", p)
+					h.RunHttpServer(ports)
+					return nil
+				},
+			},
+		},
+		Authors: []*cli.Author{{
+			Name:  "Hanan",
+			Email: "Asyrawi",
+		}},
+		Copyright: "readmanga",
+	}
+
+	// command run
+	if err := app.Run(os.Args); err != nil {
+		log.Err(err).Msg("")
+	}
 }
