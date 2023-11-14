a5b8f94842faef33c8555d9406d0430019fb6e6c
asyrawih <hasyrawi@gmail.com>
2023-11-15 01:50:08 +0700

    wip(dashboard): update add manga pages

--- dashboard/app/dashboard/manga/add/page.tsx
+++ dashboard/app/dashboard/manga/add/page.tsx
@@ -1,7 +1,17 @@
+import { AddFormManga } from "@/components/form/addmanga";
+import { Card, CardHeader, CardContent } from "@/components/ui/card";
+
 export default function AddManga() {
   return (
-    <div>
-      Add pages
+    <div className="flex justify-center w-full">
+      <Card className="w-full mt-2">
+        <CardHeader>
+          Add New Manga
+        </CardHeader>
+        <CardContent>
+          <AddFormManga />
+        </CardContent>
+      </Card>
     </div>
   )
 }
