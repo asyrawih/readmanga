f2dc9eb7b4f6d7f63379f0822931203817e8f6e7
asyrawih <hasyrawi@gmail.com>
2023-11-15 16:27:05 +0700

    wip(dashboard): create dropzone area component

--- dashboard/components/dropImage/image.tsx
+++ dashboard/components/dropImage/image.tsx
@@ -1,20 +1,59 @@
 'use client'
 
 import { useMutation } from "react-query"
+import { useDropzone } from "react-dropzone"
+import { CSSProperties, useEffect, useState } from "react";
 
-const uploadThumbnail = async () => {
+
+type ImageFile = File & {
+  preview: string
 }
 
+export const DropZoneComponent = () => {
+  const [files, setFiles] = useState<Array<ImageFile>>([]);
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: {
+      'image/jpeg': [],
+      'image/png': [],
+      'image/webp': []
+    },
+    maxFiles: 2,
+    onDrop: acceptedFiles => {
+      setFiles(acceptedFiles.map(file => Object.assign(file, {
+        preview: URL.createObjectURL(file)
+      })))
+    }
+  });
 
-export const DropZone = () => {
+  const thumbs = files.map(file => (
+    <div key={file.name} className="mt-3 mb-3 flex space-x-1">
+      <div className="w-[400px] m-2">
+        <img
+          src={file.preview}
+          // Revoke data uri after image is loaded
+          onLoad={() => { URL.revokeObjectURL(file.preview) }}
+        />
+      </div>
+    </div>
+  ));
 
-  const mutate = useMutation(uploadThumbnail, {
-    onSuccess: () => { },
-    onError: () => { }
-  })
+  useEffect(() => {
+    // Make sure to revoke the data uris to avoid memory leaks, will run on unmount
+    return () => files.forEach(file => URL.revokeObjectURL(file.preview));
+  }, []);
 
   return (
-    <div>Dropzone in here</div>
-  )
+    <>
+      <div className="flex">
+        {thumbs}
+      </div>
+      <section className="container my-2 outline-dashed  outline-cyan-800 p-12 flex justify-center  ">
+        <div {...getRootProps({ className: 'dropzone' })}>
+          <input {...getInputProps()} />
+          <p>Drag ur thumbnail in here </p>
+        </div>
+      </section>
+    </>
+  );
 }
 
